5894a0e018288cec24cbfc03f3299545685aaa00
Sam Fortuin <99059050@mydavinci.nl>
2022-02-08 14:00:41 +0100

    added debug

--- passwdGen.py
+++ passwdGen.py
@@ -1,6 +1,7 @@
-from string import ascii_lowercase,ascii_uppercase
+from string import ascii_lowercase, ascii_uppercase
 from random import randint, choice, shuffle
 from shortcuts import intConvert,coloredText,color
+from sys import stdout
 
 
 def addScpecialChars(outList, debug):
@@ -8,21 +9,33 @@ def addScpecialChars(outList, debug):
     specialChars = ['@','#','$','%','&','_','?']
     for i in range(3):
         outList.append(choice(specialChars))
+    if debug:
+        stdout.write(coloredText('',color.PURPLE,'Chars in passwd: '))
+        stdout.write('3 Special, ')
     
 def addUpperCase(outList, debug):
     #adds between 2 and 6 upper case chars
-    for i in range(randint(2,6)):
+    randNum = randint(2,6)
+    for i in range(randNum):
         outList.append(choice(ascii_uppercase))
+    if debug:
+        stdout.write(f'{randNum} Upper, ')
 
 def addNumbers(outList, debug):
     #adds between 4 and 7 numbers
     nums = list(range(10))
-    for i in range(randint(4,7)):
+    randNum = randint(4,7)
+    for i in range(randNum):
         outList.append(choice(nums))
+    if debug:
+        stdout.write(f'{randNum} Numbers, ')
 
 def addLowerCase(outList, debug):
-    for i in range(20-len(outList)):
+    remaining = 20-len(outList)
+    for i in range(remaining):
         outList.append(choice(ascii_lowercase))
+    if debug:
+        stdout.write(f'{remaining+4} Lower:\n')
 
 def createPasswd(debug):
     inList = [] #creates blank list
@@ -31,18 +44,16 @@ def createPasswd(debug):
     addNumbers(inList,debug)
     addLowerCase(inList,debug)
 
-    for i in range(choice(list(range(10)))):
+    for x in range(randint(1,10)):
         shuffle(inList)
 
     for i in range(3):
         inList.insert(0,choice(ascii_lowercase))
     inList.append(choice(ascii_lowercase))
 
-    outlist = inList
-    return outlist
+    return inList
 
-
-def main(numbered=True, debug=False):
+def main(debug=False,numbered=True):
     mainList = []
     amount = intConvert(input('hoeveel wachtwoorden wilt u?\n'))
     if isinstance(amount,int) and amount > 0:
@@ -52,9 +63,17 @@ def main(numbered=True, debug=False):
             mainList.append(createPasswd(debug))
             if numbered:
                 mainList[i].insert(0,f'{i+1}. ')
+            if debug:
+                mainList[i].append("\n")
             print(*mainList[i],sep='')
     else:
         print('invalid input')
         main()
 
+# main(debug=True)
 main()
+
+
+#[ ] implement json functions for fun
+#[ ] with json also make txt that that makes it so that a newly genned file's name is one more than the previous file
+#[ ] when json remove the numbers 
---
+++ yahtzee.py
@@ -0,0 +1,5 @@
+import numpy as np
+from shortcuts import intConvert, coloredText, color
+
+#todo play yahtzee with stijn
+#todo actually make program
