8e7ce949de5bd2c23b313839517bcc4446169c0c
Sam Fortuin <99059050@mydavinci.nl>
2022-04-07 13:06:43 +0200

    added stuff

--- yahtzee.py
+++ yahtzee.py
@@ -7,21 +7,13 @@ from os import system
 scoreBoard = {
     "ones":0,
     "twos":0,
-    "threes":0,
-    "fours":0,
-    "fives":0,
-    "sixes":0,
-    "three of a kind":0,
-    "four of a kind":0,
-    "full house":0,
-    "small straight":0,
-    "large straight":0,
-    "chance":0,
-    "yahtzee":0
+    "threes":1,
+    "fours":1,
+    "fives":1,
+    "sixes":1
 }
 
 def fancyDice(*inDice):
-    #[X]zero base dict so the +1 in the write isn't needed anymore
     diceDict = {
         0:{1:' ------- ',2:' ------- ',3:' ------- ',4:' ------- ',5:' ------- ',6:' ------- '},
         1:{1:'|       |',2:'| o     |',3:'| o     |',4:'| o   o |',5:'| o   o |',6:'| o   o |'},
@@ -94,27 +86,56 @@ def gameLoop():
             if len(hand) > 0:
                 print('Your dice')
                 fancyDice(*hand)
-            keep = input('\nWhich dice do you want to keep?\n').replace(' ','').split(',')
-            for x in keep:
-                try:
-                    indexDice = keep.index(x)
-                    keep[indexDice] = int(x)
-                    nextRoll = True
-                except ValueError:
-                    print('invalid input')
-                    nextRoll = False
-                    break
+            keep = input('\nWhich dice do you want to keep?\n').replace(' ','')
+            if keep != "":
+                keep = keep.split(',')
+                for x in keep:
+                    try:
+                        indexDice = keep.index(x)
+                        keep[indexDice] = int(x)
+                        nextRoll = True
+                    except ValueError:
+                        print('invalid input')
+                        nextRoll = False
+                        break
             if not nextRoll:
                 continue
             for x in keep:
                 hand.append(x)
-            print(hand)
             break
+    system('cls')
+    print("you have:")
+    combos = []
+    for i in range(6):
+        combos.append(f'{hand.count(i+1)}x {i+1}') if hand.count(i+1) > 0 else None
+    print(*combos,sep='\n')
+    while True:
+        comboAppend = input("Which dice do you want to add to the scoreboard?\n")
+        try:
+            comboAppend = int(comboAppend)
+            if comboAppend in keep:
+                print(f'added to {comboAppend}')
+                translateionDict = {1:'ones',2:'twos',3:'threes',4:'fours',5:'fives',6:'sixes'}
+                scoreBoard[translateionDict[comboAppend]] = comboAppend*hand.count(comboAppend)
+            else:
+                print('not possible to add to scoreboard')
+                continue
+        except ValueError:
+            print("not a number")
+            continue
+        break
 
-#[ ] append hand combo's to scoreboard
-#GOOGLE how to find combo's in list of integers
-#GOOGLE class explination and can it be used to append to a dict inside a (class)function
- 
 
 if __name__ == '__main__':
-    gameLoop()
+    while True:
+        scoreCheck = list(scoreBoard.values())
+        while 0 in scoreCheck:
+            print(scoreCheck)
+            gameLoop()
+            scoreCheck = list(scoreBoard.values())
+        else:
+            print("Your score:")
+            for x,y in scoreBoard.items():
+                 print(f'{x}:   {y} points') if x == 'ones' or x == 'twos' else (print(f'{x}: {y} points') if x == 'threes' else print(f'{x}:  {y} points'))
+            print(f'Total:  {sum(list(scoreBoard.values()))} points')
+            break
